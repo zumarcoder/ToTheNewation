6156b7a1de5d6694753c3a32c39e8a24709a9367
Akash Verma <akash.verma@pw.live>
2024-08-05 16:38:10 +0530

    new branch

--- ToTheNewation/Controller/EmployeeListViewController.swift
+++ ToTheNewation/Controller/EmployeeListViewController.swift
@@ -67,7 +67,7 @@ class EmployeeListViewController: UIViewController , UITextFieldDelegate , NSFet
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
         searchController.searchBar.returnKeyType = UIReturnKeyType.done
-    }
+    } 
     
     override func viewDidDisappear(_ animated: Bool) {
     }
@@ -77,7 +77,7 @@ class EmployeeListViewController: UIViewController , UITextFieldDelegate , NSFet
     }
 
     func getData()
-    { 
+    {
         print("1")
         DispatchQueue.main.async {
             let url = URL(string: "http://dummy.restapiexample.com/api/v1/employees")
